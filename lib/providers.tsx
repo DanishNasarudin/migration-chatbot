2b758b54a4fb7f9323abdee9d8dbea63412746cf
Danish Nasarudin <86049685+DanishNasarudin@users.noreply.github.com>
2025-09-14 14:00:40 +0800

    init

---
+++ lib/providers.tsx
@@ -0,0 +1,16 @@
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "./providers/theme-provider";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+      <Toaster richColors closeButton />
+    </ThemeProvider>
+  );
+}
